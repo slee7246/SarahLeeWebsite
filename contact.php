e34d93fec0adc33ab7d58fd923fef916c4925350
Sarah <saeheel2@illinois.edu>
2020-01-28 15:42:19 -0600

    contact inquiry

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php>
+
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_Post{'message'];
+
+$email_from = 'saeheel2@illinois.edu';
+$email_subject = "Web Page Inquiry";
+$email_body = "Name: $name"
+
+
+
+
+?>
