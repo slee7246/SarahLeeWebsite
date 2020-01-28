154dae5deb990d1bbaacb4b765ad7884eaa079ef
Sarah <saeheel2@illinois.edu>
2020-01-28 16:24:52 -0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,14 +1,25 @@
 <?php>
 
 $name = $_POST['name'];
+$reason = $_POST['reason'];
 $visitor_email = $_POST['email'];
 $message = $_Post{'message'];
 
-$email_from = 'saeheel2@illinois.edu';
+$email_from = 'https://slee7246.github.io/Sarah-sPersonalWeb/';
 $email_subject = "Web Page Inquiry";
-$email_body = "Name: $name"
+$email_body = "Name: $name.\n".
+                "Reason: $reason.\n".
+                 "User Email: $visitor_email.\n".
+                 "User Message: $message.\n";
+                  
+$to = "saheel2@Illinois.edu";
+$headers = "From: $email_from\r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
+mail($to,$email_subject,$email_body,$headers);
 
+header("Location: index.html");
+ 
 
 
 ?>
